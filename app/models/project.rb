f1a3bec6dd48fd6b3ff09bc9224a5c47895bab36
Mauro Fermani <fermani@localhost.localdomain>
2012-10-17 00:50:30 -0300

    fix eliminacion de projecto

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,7 +4,7 @@ class Project < ActiveRecord::Base
   validates :name, :uniqueness => true
   validates :description, :presence => true
 
-  has_many :permissions
+  has_many :permissions, :dependent => :delete_all
   has_many :users, :through => :permissions
   has_many :types, :through => :permissions
 end
